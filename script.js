29788e0317b65fd6d9f2581bdf191333efd0e553
bluelotus03 <25125692+bluelotus03@users.noreply.github.com>
2023-04-23 19:41:06 -0400

    sad animation

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ const spriteHeight = 523;
 gameFrame = 0;
 
 // Will slow down animation by that amount -- higher the number, the slower the animation will be 
-const staggerFrames = 28;
+const staggerFrames = 24;
 
 // Stores all the positions of sprites for a given animation (row) as calculated in forEach() below
 const spriteAnimations = [];
